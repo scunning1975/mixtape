4eb637d7436d00b3eddc9503ac940ed5ab609fc3
be-green <brice.c.green@gmail.com>
2020-04-05 13:15:40 -0700

    remove code that was already run in castle_2.R & redundant paste() command

--- programs/castle_3.R
+++ programs/castle_3.R
@@ -1,26 +1,3 @@
-castle <- castle %>%
-  mutate(
-    time_til = year - treatment_date,
-    lead1 = case_when(time_til == -1 ~ 1, TRUE ~ 0),
-    lead2 = case_when(time_til == -2 ~ 1, TRUE ~ 0),
-    lead3 = case_when(time_til == -3 ~ 1, TRUE ~ 0),
-    lead4 = case_when(time_til == -4 ~ 1, TRUE ~ 0),
-    lead5 = case_when(time_til == -5 ~ 1, TRUE ~ 0),
-    lead6 = case_when(time_til == -6 ~ 1, TRUE ~ 0),
-    lead7 = case_when(time_til == -7 ~ 1, TRUE ~ 0),
-    lead8 = case_when(time_til == -8 ~ 1, TRUE ~ 0),
-    lead9 = case_when(time_til == -9 ~ 1, TRUE ~ 0),
-    
-    lag0 = case_when(time_til == 0 ~ 1, TRUE ~ 0),
-    lag1 = case_when(time_til == 1 ~ 1, TRUE ~ 0),
-    lag2 = case_when(time_til == 2 ~ 1, TRUE ~ 0),
-    lag3 = case_when(time_til == 3 ~ 1, TRUE ~ 0),
-    lag4 = case_when(time_til == 4 ~ 1, TRUE ~ 0),
-    lag5 = case_when(time_til == 5 ~ 1, TRUE ~ 0)
-  )
-
-paste(paste("lead", 1:9, sep = ""), collapse = " + ")
-paste(paste("lag", 1:5, sep = ""), collapse = " + ")
 
 leadslags_plot <- tibble(
   sd = c(reg3$cse[53:45], reg3$se[54:58]),
