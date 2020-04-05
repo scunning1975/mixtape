2745af169330c89d854d39ff2b4b11c17e21b444
be-green <brice.c.green@gmail.com>
2020-04-05 14:51:43 -0700

    update castle code for colinearity, update plotting code

--- programs/castle_4.R
+++ programs/castle_4.R
@@ -1,11 +1,19 @@
-leadslags_plot <- rbind(leadslags_plot, c(0,0,0))
 
+# This version includes
+# an interval that traces the confidence intervals
+# of your coefficients
 leadslags_plot %>%
-  ggplot(aes(x = label, y = mean)) +
-  geom_point()+
-  geom_hline(yintercept = 0, color = "black") +
-  geom_vline(xintercept = 0, color = "black") +
+  ggplot(aes(x = label, y = mean,
+             ymin = mean-1.96*sd, 
+             ymax = mean+1.96*sd)) +
+  # this creates a red horizontal line
   geom_hline(yintercept = 0.035169444, color = "red") +
-  geom_line() +
-  geom_ribbon(aes(ymin = mean-sd, ymax = mean+sd), alpha = 0.2,
-              position = position_dodge(0.05))
+  geom_line() + 
+  geom_point() +
+  geom_ribbon(alpha = 0.2) +
+  theme_minimal() +
+  # Important to have informative axes labels!
+  xlab("Years before and after castle doctrine expansion") +
+  ylab("log(Homicide Rate)") +
+  geom_hline(yintercept = 0) +
+  geom_vline(xintercept = 0)
--- programs/castle_5.R
+++ programs/castle_5.R
@@ -8,16 +8,17 @@ df_bacon <- bacon(l_homicide ~ post,
 summary(df_bacon)
 
 
-#--
+# create formula
+# drop one additional region variable due to colinearity
 formula <- as.formula(
   paste("l_homicide ~ post + lead9 + lead8 + lead7 + ",
         "lead6 + lead5 + lead4 + lead3 + lead2 + lead1 + ",
         "lag1 + lag2 + lag3 + lag4 + lag5 + ",
         paste(
           paste(xvar, collapse = " + "),
-          paste(region, collapse = " + "),
-          paste(lintrend, collapse = " + "),
-          paste("post", collapse = " + "), sep = " + "),
+          paste(subset(region, region != 'r20003'), 
+                collapse = " + "),
+          paste(lintrend, collapse = " + "), sep = " + "),
         "| year + sid"
   )
 )
