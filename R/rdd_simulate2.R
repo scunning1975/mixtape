433184762ab8f1b9de6de670eb41d4a2c225512a
Kyle F Butts <buttskyle96@gmail.com>
2021-02-10 16:35:18 -0700

    Correct figure numbering

--- R/rdd_simulate2.R
+++ R/rdd_simulate2.R
@@ -4,7 +4,7 @@ dat <- dat %>%
     y2 = 25 + 40 * D + 1.5 * x + rnorm(n(), 0, 20)
   )
 
-# figure 36
+# figure
 ggplot(aes(x, y2, colour = factor(D)), data = dat) +
   geom_point(alpha = 0.5) +
   geom_vline(xintercept = 50, colour = "grey", linetype = 2) +
