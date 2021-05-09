ebdebd510adb81c363de8fde84430d3459f73083
alexanderthclark <alexanderthclark@users.noreply.github.com>
2021-05-09 12:58:13 -0400

    Update nsw_pscore.py
    Added identity I() around quadratic and cubic terms because of issues with patsy formulas so these are included in the regression. Also modified u74 and u75 definition as the previous form didn't work for me.

--- python/nsw_pscore.py
+++ python/nsw_pscore.py
@@ -16,11 +16,11 @@ def read_data(file):
 nsw_dw_cpscontrol = read_data('cps_mixtape.dta')
 
 nsw_dw_cpscontrol = pd.concat((nsw_dw_cpscontrol, nsw_dw))
-nsw_dw_cpscontrol[['u74', 'u75']] = 0
+nsw_dw_cpscontrol['u74'], nsw_dw_cpscontrol['u75'] = 0, 0
 nsw_dw_cpscontrol.loc[nsw_dw_cpscontrol.re74==0, 'u74'] = 1
 nsw_dw_cpscontrol.loc[nsw_dw_cpscontrol.re75==0, 'u75'] = 1
 # estimating propensity score
-logit_nsw = smf.glm(formula="""treat ~ age + age**2 + age**3 + educ + educ**2 + 
+logit_nsw = smf.glm(formula="""treat ~ age + I(age**2) + I(age**3) + educ + I(educ**2) + 
                     marr + nodegree + black + hisp + re74 + re75 + u74 + u75 + educ*re74""", 
                     family=sm.families.Binomial(),
                    data=nsw_dw_cpscontrol).fit()
